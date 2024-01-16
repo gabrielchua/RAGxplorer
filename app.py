9ba9c12f0f39fdaeac48af0325db6872cb05c0ba
Gabriel Chua <cyzgab@gmail.com>
2024-01-17 00:16:12 +0800

    fix sqlite

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import pandas as pd
