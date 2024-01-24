250c0635d45359a014058c6ac673eeee28eaabee
Ted Stresen-Reuter <ted@secret-source.eu>
2024-01-24 13:09:33 +0000

    Remove streamlit hosting reqs
    Commented out the imports for the streamlit hosting because
    pysqlite3 is unavailable for M1 arch.

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 Streamlit app
 """
 # Line 5 to 7 is for streamlit commmunity deployment
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import pandas as pd
