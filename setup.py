863070b7f6f010b0b90b5b8a62f93544bf2f202d
Gabriel Chua <cyzgab@gmail.com>
2024-02-04 21:58:50 +0800

    update versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ragxplorer',
-    version='0.1.8',
+    version='0.1.9',
     author='Gabriel Chua',
     author_email='cyzgab@gmail.com',
     description='A open-source tool to to visualise your RAG documents 🔮.',
