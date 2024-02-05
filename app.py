2c87b506e16000f9f41fa43d5ee22b670875d5c9
gabriel chua <34762009+gabrielchua@users.noreply.github.com>
2024-02-05 09:33:26 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+"""
+placeholder file for original streamlit app hosted on community cloud
+"""
+
+import streamlit as st
+
+st.title("The demo has been moved to [here](https://ragxplorer.streamlit.app/)")
